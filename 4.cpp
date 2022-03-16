af467faafc4d113d5e91039d0966a466324b3062
iamgauravsharma2084 <79034287+iamgauravsharma2084@users.noreply.github.com>
2022-03-16 10:51:26 -0700

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class complex
+{
+   private:
+	 int ro;
+   public:
+   	complex(int r)
+   	{
+		 int s=50ca;
+		 cout<<"Enter your numbar:-";
+		 cin>>ro;
+		 a=s+ro;
+		 cout<<a;
+    }
+    complex()
+    {
+     // void fun()
+     // {
+		 cout<<"Gaurav sharma";
+     // }
+  }
+};
+main()
+{
+	 complex c1(0),c2;
+
+	 
+}
